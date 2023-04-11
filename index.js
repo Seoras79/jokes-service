b5e166ad38d0add1a411387377efcbe64ebf447e
Seoras79 <seorasproductions@gmail.com>
2023-04-11 11:29:42 +0100

    created route to find by query tags or content

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const app = express();
 const { Joke } = require('./db');
 
+app.set("json spaces", "\t")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
+  if(req.query) {
+    try {
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]: req.query.tags ?? '',
+          },
+          joke: {
+            [Op.substring]: req.query.content ?? '',
+            },
+          },
+        });
+        res.send(jokes);
+      } catch (error) {
+          console.error(error);
+          next(error)
+      }
+      return;
+  }
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
+      const jokes = await Joke.findAll(); // SELECT * FROM jokes;
+      res.send(jokes);
   } catch (error) {
-    console.error(error);
-    next(error)
+      console.error(error);
+      next(error)
   }
 });
 
